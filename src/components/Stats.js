0c256804c2bd64a22b8ca8ed2d71ea2e03b5376d
Ieva Vyliaudaite <ievavyl@yahoo.com>
2020-11-24 14:28:55 +0200

    saving progress

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,34 +1,29 @@
-const Stats =({countries, worldPopulation}) => {
+import '../styles/stats.css'
 
+const Stats =({countries, worldPopulation}) => {
 //sort array by population biggest->smallest
 //splice top 10
 //worldPopulation = 100% / 
 
-
-    let countriesSorted = 
-        countries.sort((a, b) => b.population - a.population)
-    console.log('sorted: ', countriesSorted);
-
     return (
         <div className='stats-wrapper'>
-            <div className='country-column'>
-                WORLD: {worldPopulation}
+            <div className='world-column'>
+                <div className='column-titles'>
+                    <p>WORLD</p> <p style={{ paddingRight:'0.5rem' }}>{worldPopulation}</p>
+                </div>
             </div>
-            {countriesSorted.slice(0, 10)
+            {countries.sort((a, b) => b.population - a.population).slice(0, 10)
                 .map((country, i) => 
-                    <div key={i} className='country-column'
-                        style={{
-                            width: `${country.population/1000}%`, 
-                            hegiht:'2rem'
-                        }}
-                    >
-                        {country.name} {country.population}
+                    <div key={i} className='country-column'> 
+                        <div className='inner-column' style={{width:`${10}%`}}> </div>  
+                        <div className='column-titles'>
+                            <p>{country.name}</p> <p>{country.population}</p>
+                        </div>
+                         
                     </div>
                 )
             }
         </div>        
-    )
-    
+    )    
 }
-
 export default Stats
